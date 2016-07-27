833831870aca67142066bbc4621e7cf1b5078db0
chenjina <18978323462@163.com>
2016-07-27 13:05:22 +0800

    创建项目 集成masonry

--- masnoryDemo/ViewController.m
+++ masnoryDemo/ViewController.m
@@ -18,7 +18,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
+#pragma mark 初始化子视图
+-(void)setUI{
 
+
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
